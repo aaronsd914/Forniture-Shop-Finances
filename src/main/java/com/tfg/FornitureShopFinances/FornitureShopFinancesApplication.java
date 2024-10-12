ccf7b35040c4a6c3236e27dd22359f051f07cdbf
aaronsd914 <116206462+aaronsd914@users.noreply.github.com>
2024-10-12 17:45:11 +0200

    first commit

---
+++ src/main/java/com/tfg/FornitureShopFinances/FornitureShopFinancesApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.FornitureShopFinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornitureShopFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornitureShopFinancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfg/FornitureShopFinances/FornitureShopFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.FornitureShopFinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornitureShopFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
